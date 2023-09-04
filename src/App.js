deff1dd15c6d6eee3f5c8f175de4470b0b0fef20
fabloy <fabioloyacono.communication@gmail.com>
2023-09-04 19:51:22 +0200

    seconda commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import './App.css';
 import Calculator from './components/Calculator';
 
 function App() {
 
   
  return (
-    <div className="App">
+    <div>
      <h1 
       className='text-primary m-3 text-center text-md-start'
      >
--- src/components/mini-components/MainCalc.js
+++ src/components/mini-components/MainCalc.js
@@ -2,7 +2,7 @@ import Button from 'react-bootstrap/Button';
 import ListGroup from 'react-bootstrap/ListGroup';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-let listGroupItemCSSDefault = " w-75 d-flex flex-column flex-md-row justify-content-center align-items-center mb-1"
+
 const MainCalc = ({elements,editOperator, editElement, disabledElement, deleteElement})=>{
     return(
         <section className="mb-5 d-flex align-items-start my-max-height">
@@ -15,13 +15,13 @@ const MainCalc = ({elements,editOperator, editElement, disabledElement, deleteEl
                          key={index}
                          className={`
                           ${!el.enabled ? "bg-secondary " : "bg-light"} 
-                          ${listGroupItemCSSDefault}
+                          w-100 d-flex flex-column flex-md-row justify-content-center align-items-center mb-1
                         `}
                         >
-                        <div className='text-center  me-md-2' >
+                        <div className="col-7 col-md-4 text-center  me-md-2 d-flex justify-content-between justify-content-md-evenly " >
                          <select
                           onChange={ e=>editOperator(e.target.value, el) }
-                          className='m-1'
+                          className='col-2 '
                          >
                           <option selected>+</option>
                           <option>-</option>
@@ -32,21 +32,22 @@ const MainCalc = ({elements,editOperator, editElement, disabledElement, deleteEl
                           value={el.value}
                           onInput={ e=>editElement(e.target.value, el) }
                           placeholder="Insert a number"
+                          className='col-9 p-1'
                          />
                          </div>
 
-                         <div className='text-center '>
+                         <div className="col-7 col-md-5 d-flex justify-content-center">
                          <Button
                           variant={el.enabled ? "secondary" : "success" }
                           onClick={ ()=>disabledElement(el) }
-                          className="m-1"
+                          className="m-1 col-6"
                          >
                           {el.enabled ? "Disabled" : "Enabled" } 
                          </Button>
                          <Button
                           variant="danger"
                           onClick={ ()=>deleteElement(el) }
-                          className="m-1"
+                          className="m-1 col-6"
                          >
                           Delete
                          </Button>
